e3621a79fb1be0aa037670d4567b6837424377fc
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-09-13 11:45:04 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Justin Hold on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
