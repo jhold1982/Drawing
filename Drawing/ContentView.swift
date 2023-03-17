fde613b50056ca5f7e3fc397b74989a5f24e5622
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-16 22:14:50 -0500

    Initial commit

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -4,21 +4,66 @@
 //
 //  Created by Justin Hold on 9/13/22.
 //
-
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Arrow: Shape {
+    var headHeight = 0.5
+    var shaftWidth = 0.5
+    var animatableData: AnimatablePair<Double, Double> {
+        get { AnimatablePair(headHeight, shaftWidth) }
+        set {
+            headHeight = newValue.first
+            shaftWidth = newValue.second
         }
-        .padding()
+    }
+    func path(in rect: CGRect) -> Path {
+        let height = rect.height * headHeight
+        let thickness = rect.width * shaftWidth / 2
+        return Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: 0))
+            path.addLine(to: CGPoint(x: rect.maxX, y: height))
+            path.addLine(to: CGPoint(x: rect.midX + thickness, y: height))
+            path.addLine(to: CGPoint(x: rect.midX + thickness, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX - thickness, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX - thickness, y: height))
+            path.addLine(to: CGPoint(x: 0, y: height))
+            path.closeSubpath()
+        }
     }
 }
-
+struct ColorCyclingArrow: View {
+    var amount = 0.0
+    var steps = 100
+    var body: some View {
+        ZStack {
+            ForEach(0..<100) { value in
+                Arrow()
+                    
+            }
+        }
+        .drawingGroup()
+    }
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+struct ContentView: View {
+    @State private var headHeight = 0.5
+    @State private var shaftWidth = 0.25
+    var body: some View {
+        Arrow(headHeight: headHeight, shaftWidth: shaftWidth)
+            .fill(.blue)
+            .onTapGesture {
+                withAnimation {
+                    headHeight = Double.random(in: 0.2...0.8)
+                    shaftWidth = Double.random(in: 0.2...0.8)
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
